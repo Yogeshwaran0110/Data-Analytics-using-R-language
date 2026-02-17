8ad4f8e73545cd4b7cb6877ec864fd9af69ad0e4
Yogeshwaran0110 <yogeshwaranmuruganandam187@gmail.com>
2026-02-17 19:58:23 +0530

    Add files via upload

---
+++ OTT watch hours plot.R
@@ -0,0 +1,12 @@
+ott <- data.frame(
+  day = 1:12,
+  watch_hours = c(1.5, 2, 2.2, 3, 3.5, 4, 4.2, 5, 5.5, 6, 6.5, 7)
+)
+plot(
+  ott$day,
+  ott$watch_hours,
+  type = "l",
+  xlab = "Days",
+  ylab = "Watch Hours",
+  main = "OTT Watch Hours Trend",
+)
